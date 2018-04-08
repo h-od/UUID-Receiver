753180e8b9dcc0568d46859185d26f8f6c7630d6
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:16:04 +0100

    added contracts for managing data

--- app/src/main/java/com/hod/uuidreceiver/data/DataSource.kt
+++ app/src/main/java/com/hod/uuidreceiver/data/DataSource.kt
@@ -1,2 +1,28 @@
 package com.hod.uuidreceiver.data
 
+import com.google.gson.annotations.SerializedName
+import io.reactivex.Observable
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DataContract {
+    fun fetch(): Observable<Entity>
+    fun fetchPrevious(): Single<Entity>
+}
+
+interface Remote {
+    @GET("{path}")
+    fun fetch(@Path("path") string: String = ""): Observable<Entity>
+}
+
+interface Local {
+    fun fetch(): Entity
+    fun put(code: String): Entity
+}
+
+data class Entity(
+        @SerializedName("next_path") val nextPath: String = "",
+        @SerializedName("path") val path: String = "",
+        @SerializedName("response_code") val responseCode: String = "",
+        val count: Int = 0)
