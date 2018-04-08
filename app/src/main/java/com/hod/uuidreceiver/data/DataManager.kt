1e523447b2a797805e197acf41e4280a1c0dd42f
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:16:53 +0100

    added data implementation for fetching data from network and fetching the last one from local storage

--- app/src/main/java/com/hod/uuidreceiver/data/DataManager.kt
+++ app/src/main/java/com/hod/uuidreceiver/data/DataManager.kt
@@ -1,2 +1,19 @@
 package com.hod.uuidreceiver.data
 
+import com.hod.uuidreceiver.BuildConfig
+import io.reactivex.Observable
+import io.reactivex.Single
+
+class DataManager(private val local: Local, private val network: Remote): DataContract {
+
+    override fun fetch(): Observable<Entity> =
+            network.fetch()
+                    .map { it.nextPath.removePrefix(BuildConfig.BASE_URL) }
+                    .flatMap { network.fetch(it) }
+                    .map { local.put(it.responseCode) }
+
+    override fun fetchPrevious(): Single<Entity> {
+        val entity = local.fetch()
+        return Single.just(Entity(responseCode = entity.responseCode, count = entity.count))
+    }
+}
