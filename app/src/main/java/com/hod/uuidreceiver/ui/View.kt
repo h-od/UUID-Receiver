57bf3e8483cb2ff1aa1c79e0103a47c944748d66
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:49:24 +0100

    added view implementation

--- app/src/main/java/com/hod/uuidreceiver/ui/View.kt
+++ app/src/main/java/com/hod/uuidreceiver/ui/View.kt
@@ -1,39 +1,69 @@
 package com.hod.uuidreceiver.ui
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import com.hod.uuidreceiver.App
 import com.hod.uuidreceiver.R
+import com.jakewharton.rxbinding2.view.clicks
 import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MainPresenter.View {
+
+    @Inject lateinit var presenter: MainPresenter
+
+    private val errorDismissedPublishSubject: PublishSubject<Unit> = PublishSubject.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initialiseView()
+        presenter.onViewAttached(this)
+    }
+
+    override fun onDestroy() {
+        presenter.onViewDetached(this)
+        super.onDestroy()
+    }
+
+    override fun fetchClicked(): Observable<Unit> = fetch.clicks().debounce(1, TimeUnit.SECONDS)
+
+    override fun errorDismissed(): Observable<Unit> = errorDismissedPublishSubject
+
+    override fun showCount(count: Int) {
+        timesFetched.text = getString(R.string.fetched_times, count.toString())
+    }
+
+    override fun showResponseCode(code: String) {
+        responseCode.text = code
+    }
+
+    override fun showNoResponseCode() {
+        responseCode.text = getString(R.string.no_response_code)
+    }
+
+    override fun showError(errorMessage: String?) {
+        AlertDialog.Builder(this)
+                .customise(errorMessage)
+                .create()
+                .show()
+    }
+
+    private fun initialiseView() {
+        (application as App).component.inject(this)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
     }
 
-    override fun fetchClicked(): Observable<Unit> {
-        TODO("provide presenter with click events")
-    }
-
-    override fun errorDismissed(): Observable<Unit> {
-        TODO("provide presenter with error dismissed events")
-    }
-
-    override fun showCount(count: Int) {
-        TODO("provide view with count data to show")
-    }
-
-    override fun showResponseCode(code: String) {
-        TODO("provide view with response code data to show")
-    }
-
-    override fun showNoResponseCode() {
-        TODO("let view know that there is no response code to show")
-    }
-
-    override fun showError(errorMessage: String?) {
-        TODO("inform user about error that occurred")
+    private fun AlertDialog.Builder.customise(errorMessage: String?): AlertDialog.Builder {
+        if (errorMessage.isNullOrBlank()) {
+            setMessage(R.string.error)
+        } else {
+            setTitle(R.string.error).setMessage(errorMessage)
+        }
+        return setOnDismissListener { errorDismissedPublishSubject.onNext(Unit) }
     }
 }
