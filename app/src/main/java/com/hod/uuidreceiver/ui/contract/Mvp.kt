ec7516a5572cfce9fe1e1ee038b90b7323271eeb
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:18:31 +0100

    added contract for view-presentation interactions

---
+++ app/src/main/java/com/hod/uuidreceiver/ui/contract/Mvp.kt
@@ -0,0 +1,35 @@
+package com.hod.uuidreceiver.ui.contract
+
+import android.support.annotation.CallSuper
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class AbsPresenter<in V : View> {
+
+    private val viewSubscriptions by lazy { CompositeDisposable() }
+
+    abstract fun onViewAttached(view: V)
+
+    @CallSuper
+    open fun onViewDetached(view: V) {
+        this.viewSubscriptions.clear()
+    }
+
+    private fun clearOnDetached(disposable: Disposable) {
+        this.viewSubscriptions.add(disposable)
+    }
+
+    protected fun <T> Observable<T>.subscribeUntilDetached(onNext: (T) -> Unit): Disposable =
+            subscribe(onNext).apply { clearOnDetached(this) }
+
+    protected fun <T> Observable<T>.subscribeUntilDetached(onNext: (T) -> Unit, onError: (Throwable) -> Unit): Disposable =
+            subscribe(onNext, onError).apply { clearOnDetached(this) }
+
+    protected fun <T> Single<T>.subscribeUntilDetached(onNext: (T) -> Unit, onError: (Throwable) -> Unit): Disposable =
+            subscribe(onNext, onError).apply { clearOnDetached(this) }
+
+}
+
+interface View
