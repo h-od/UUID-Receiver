581220f506d1e53d34aefbfa4040b3fd09a5a279
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:23:09 +0100

    added layout

--- app/src/main/java/com/hod/uuidreceiver/ui/View.kt
+++ app/src/main/java/com/hod/uuidreceiver/ui/View.kt
@@ -1,2 +1,15 @@
 package com.hod.uuidreceiver.ui
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hod.uuidreceiver.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+}
