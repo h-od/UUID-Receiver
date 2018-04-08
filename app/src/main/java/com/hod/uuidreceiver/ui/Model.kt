444bcff3c962c9f11ee5a8fd1294b9bf9f48d860
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:17:32 +0100

    added model for holding data to display in the view

--- app/src/main/java/com/hod/uuidreceiver/ui/Model.kt
+++ app/src/main/java/com/hod/uuidreceiver/ui/Model.kt
@@ -1,2 +1,7 @@
 package com.hod.uuidreceiver.ui
 
+import com.hod.uuidreceiver.data.Entity
+
+data class Model(val code: String, val count: Int)
+
+fun Entity.toModel(): Model = Model(responseCode, count)
