c3c7155a8f1a97a5e23fe8a30724802af025e6a8
hugh <hugh.odonnell@tigerspike.com>
2018-04-08 16:52:55 +0100

    added tests for data manager and main presenter

---
+++ app/src/test/java/com/hod/uuidreceiver/data/DataManagerTest.kt
@@ -0,0 +1,52 @@
+package com.hod.uuidreceiver.data
+
+import com.hod.uuidreceiver.BuildConfig.BASE_URL
+import io.reactivex.Observable
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.BDDMockito
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner.StrictStubs::class)
+class DataManagerTest() {
+
+    private lateinit var dataManager: DataManager
+
+    @Mock private lateinit var storage: Local
+    @Mock private lateinit var network: Remote
+
+    @Before
+    fun setUp() {
+        dataManager = DataManager(storage, network)
+    }
+
+    @Test
+    fun givenFetchIsCalledReturnsEntity() {
+        val nextPath = "nextPath"
+        val responseCode = "responseCode"
+
+        val entity = Entity(nextPath = BASE_URL + nextPath, responseCode = responseCode)
+        val result = Observable.just(entity)
+
+        BDDMockito.doReturn(result).`when`(network).fetch()
+        BDDMockito.doReturn(result).`when`(network).fetch(nextPath)
+        BDDMockito.doReturn(entity).`when`(storage).put(responseCode)
+
+        dataManager.fetch()
+                .test()
+                .assertValue(entity)
+    }
+
+    @Test
+    fun givenFetchPreviousIsCalledReturnsPreviousEntity() {
+        val entity = Entity(responseCode =  "responseCode", count = 5)
+
+        BDDMockito.doReturn(entity).`when`(storage).fetch()
+
+        dataManager.fetchPrevious()
+                .test()
+                .assertValue(entity)
+    }
+}
---
+++ app/src/test/java/com/hod/uuidreceiver/ui/MainPresenterTest.kt
@@ -0,0 +1,116 @@
+package com.hod.uuidreceiver.ui
+
+import com.hod.uuidreceiver.data.DataContract
+import com.hod.uuidreceiver.data.Entity
+import com.nhaarman.mockito_kotlin.eq
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.never
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+class MainPresenterTest {
+
+    private lateinit var presenter: MainPresenter
+
+    private val dataManager = mock<DataContract>()
+    private val view = mock<MainPresenter.View>()
+
+    private val fetchClicks: PublishSubject<Unit> = PublishSubject.create()
+    private val dismissError: PublishSubject<Unit> = PublishSubject.create()
+
+    @Before
+    fun setUp() {
+        presenter = MainPresenter(dataManager, Schedulers.trampoline())
+
+        Mockito.doReturn(fetchClicks).`when`(view).fetchClicked()
+        Mockito.doReturn(dismissError).`when`(view).errorDismissed()
+    }
+
+    @Test
+    fun `when onViewAttached called fetches previous`() {
+        val responseCode = "response code"
+        val count = 5
+        val previousEntity = Single.just(Entity(responseCode = responseCode, count = count))
+
+        Mockito.doReturn(previousEntity).`when`(dataManager).fetchPrevious()
+
+        presenter.onViewAttached(view)
+
+        Mockito.verify(view).showCount(eq(count))
+        Mockito.verify(view).showResponseCode(eq(responseCode))
+    }
+
+    @Test
+    fun `when onViewAttached called with blank responseCode and non positive count calls showNoResponseCode`() {
+        val responseCode = ""
+        val count = 0
+        val previousEntity = Single.just(Entity(responseCode = responseCode, count = count))
+
+        Mockito.doReturn(previousEntity).`when`(dataManager).fetchPrevious()
+
+        presenter.onViewAttached(view)
+
+        Mockito.verify(view).showNoResponseCode()
+        Mockito.verify(view, never()).showCount(eq(count))
+        Mockito.verify(view, never()).showResponseCode(eq(responseCode))
+    }
+
+    @Test
+    fun `when error occurs calls showError`() {
+        val errorMessage = "errorMessage"
+        val previousEntity = Single.error<Exception>(Exception(errorMessage))
+
+        Mockito.doReturn(previousEntity).`when`(dataManager).fetchPrevious()
+
+        presenter.onViewAttached(view)
+
+        Mockito.verify(view).showError(eq(errorMessage))
+    }
+
+    @Test
+    fun `when fetch clicked returns Entity`() {
+
+        val previousResponseCode = "previousResponseCode"
+        val previousCount = 5
+        val previousEntity = Single.just(Entity(responseCode = previousResponseCode, count = previousCount))
+
+        val responseCode = "response code"
+        val count = 6
+        val entity = Observable.just(Entity(responseCode = responseCode, count = count))
+
+        Mockito.doReturn(previousEntity).`when`(dataManager).fetchPrevious()
+        Mockito.doReturn(entity).`when`(dataManager).fetch()
+
+        presenter.onViewAttached(view)
+
+        fetchClicks.onNext(Unit)
+
+        Mockito.verify(dataManager).fetch()
+        Mockito.verify(view).showCount(eq(previousCount))
+        Mockito.verify(view).showResponseCode(eq(previousResponseCode))
+        Mockito.verify(view).showCount(eq(count))
+        Mockito.verify(view).showResponseCode(eq(responseCode))
+    }
+
+    @Test
+    fun `when error dismissed resubscribes for clicks`() {
+
+        val previousResponseCode = "previousResponseCode"
+        val previousCount = 5
+        val previousEntity = Single.just(Entity(responseCode = previousResponseCode, count = previousCount))
+
+        Mockito.doReturn(previousEntity).`when`(dataManager).fetchPrevious()
+
+        presenter.onViewAttached(view)
+
+        dismissError.onNext(Unit)
+        fetchClicks.onNext(Unit)
+
+        Mockito.verify(dataManager, Mockito.times(2)).fetch()
+    }
+}
