da059844b84818dd086e7625e83b981178efe27c
hugh <hugh.odonnell@tigerspike.com>
2018-04-09 17:38:25 +0100

    removed redundant @Inject, it's already provided; changed shared prefs access

--- app/src/main/java/com/hod/uuidreceiver/data/LocalStorage.kt
+++ app/src/main/java/com/hod/uuidreceiver/data/LocalStorage.kt
@@ -1,9 +1,8 @@
 package com.hod.uuidreceiver.data
 
 import android.content.SharedPreferences
-import javax.inject.Inject
 
-class LocalStorage @Inject constructor(private val sharedPreferences: SharedPreferences) : Local {
+class LocalStorage(private val sharedPreferences: SharedPreferences) : Local {
 
     companion object {
         private const val CODE = "KEY_CODE"
@@ -17,10 +16,11 @@ class LocalStorage @Inject constructor(private val sharedPreferences: SharedPref
     override fun put(code: String): Entity {
         val entity = Entity(responseCode = code, count = sharedPreferences.getInt(COUNT, 0) + 1)
 
-        sharedPreferences.edit()
-                .putString(CODE, entity.responseCode)
-                .putInt(COUNT, entity.count)
-                .apply()
+        sharedPreferences.edit().apply {
+            putString(CODE, entity.responseCode)
+            putInt(COUNT, entity.count)
+            apply()
+        }
 
         return entity
     }
